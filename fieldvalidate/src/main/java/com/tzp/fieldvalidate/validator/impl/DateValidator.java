7a502eb00e1a60e7bf9ac09d77262a8b160ce058
孙玥 <373599331@qq.com>
2017-01-13 09:53:06 +0800

    提交

--- fieldvalidate/src/main/java/com/tzp/fieldvalidate/validator/impl/DateValidator.java
+++ fieldvalidate/src/main/java/com/tzp/fieldvalidate/validator/impl/DateValidator.java
@@ -7,28 +7,30 @@ import java.util.List;
 
 import com.tzp.fieldvalidate.validator.ReturnValueValidator;
 
-public class DateValidator implements ReturnValueValidator{
-	private String formatPattern;
-	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	
-	public Object validate(String fieldName, Object fieldValue, String rule){
-		if(fieldValue != null && fieldValue.getClass().isArray()){
-			fieldValue = ((Object[])fieldValue)[0];
-		}else if(fieldValue != null && fieldValue instanceof List){
-			fieldValue = ((List)fieldValue).get(0);
-		}
-		
-		Date date = null;
-		try {
-			date = dateFormat.parse(String.valueOf(fieldValue));
-		} catch (ParseException e) {
-			throw new RuntimeException(String.format("can't parse string【%s】 to date using the pattern【%s】", fieldValue,formatPattern));
-		}
-		return date;
-	}
-	
-	public void setFormatPattern(String formatPattern) {
-		this.formatPattern = formatPattern;
-		dateFormat.applyPattern(formatPattern);
-	}
+public class DateValidator implements ReturnValueValidator {
+    private String formatPattern;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    //注释
+    public Object validate(String fieldName, Object fieldValue, String rule) {
+        if (fieldValue != null && fieldValue.getClass().isArray()) {
+            fieldValue = ((Object[]) fieldValue)[0];
+        } else if (fieldValue != null && fieldValue instanceof List) {
+            fieldValue = ((List) fieldValue).get(0);
+        }
+
+        Date date = null;
+        try {
+            date = dateFormat.parse(String.valueOf(fieldValue));
+        } catch (ParseException e) {
+            throw new RuntimeException(String.format(
+                "can't parse string【%s】 to date using the pattern【%s】", fieldValue, formatPattern));
+        }
+        return date;
+    }
+
+    public void setFormatPattern(String formatPattern) {
+        this.formatPattern = formatPattern;
+        dateFormat.applyPattern(formatPattern);
+    }
 }
